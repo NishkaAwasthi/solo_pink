cc8e2b19383ad7532f8a316250772d6aa14da9ff
NishkaAwasthi <nishkaawasthi@gmail.com>
2024-02-18 17:06:45 -0500

    splay 2

--- dynamic programming/bakery.cpp
+++ dynamic programming/bakery.cpp
@@ -1,74 +1,68 @@
-#include <stdio.h>
-#include <algorithm>
-#include <memory.h>
-#include <cassert>
 #include <iostream>
-#include <cmath>
+#include <unordered_set>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-const int N = 3.5e4 + 5;
-const int K = 50 + 5;
+const int N = 50 + 5;
+const int K = 35000 + 5;
+const int nINF = -50000;
+int a[N];
+int dp[N][K];
 
-int n, k, ans;
-int a[N], last[N];
-int f[N][K];
+int value(int k, int j) {
+    unordered_set<int> uniqueValues;
+    for (int i = k; i < j+1; i++) {
+        uniqueValues.insert(a[i]);
+    }
+    return uniqueValues.size();
+}
 
-struct Segment_Tree {
-#define ls (p << 1)
-#define rs (p << 1 | 1)
-#define mid ((l + r) >> 1)
-    int max[N << 2], tag[N << 2];
-    void push_up(int p) { max[p] = std::max(max[ls], max[rs]); }
+int recurse(int boxes, int cakes) {
+    if (dp[boxes][cakes] != -1) { 
+        return dp[boxes][cakes]; 
+    }
+
+     //only one cake and many boxes
+    if (cakes == 0 && boxes > 0) {
+        dp[boxes][cakes] = nINF;
+    }
     
-    void push_down(int p) {
-        if (tag[p])
-        {
-            max[ls] += tag[p], max[rs] += tag[p];
-            tag[ls] += tag[p], tag[rs] += tag[p];
-            tag[p] = 0;
+    // only one box
+    else if (boxes == 0) {
+        dp[boxes][cakes] = value(0, cakes);
+    }
+
+    else if (boxes > 0) {   
+        //dp[i][j] = max(dp[i-1][k]) + value(k+1, j)
+        for (int k = 0; k < cakes+1; k++) {
+            dp[boxes][cakes] = max(dp[boxes][cakes], recurse(boxes-1, k) + value(k+1, cakes));
         }
     }
-    
-    void modify(int p, int l, int r, int x, int y, int val) {
-        if (l == x and r == y)
-            return max[p] += val, tag[p] += val, void();
-        push_down(p);
-        if (x <= mid)
-            modify(ls, l, mid, x, std::min(mid, y), val);
-        if (mid < y)
-            modify(rs, mid + 1, r, std::max(mid + 1, x), y, val);
-        push_up(p);
-    }
-
-    int query(int p, int l, int r, int x, int y) {
-        if (l == x and r == y)
-            return max[p];
-        push_down(p);
-        int res = 0;
-        if (x <= mid)
-            res = std::max(res, query(ls, l, mid, x, std::min(mid, y)));
-        if (mid < y)
-            res = std::max(res, query(rs, mid + 1, r, std::max(mid + 1, x), y));
-        return res;
-    }
-} t[K];
+    return dp[boxes][cakes];
+}
 
 int main() {
     // get input
-    int n, k;
-    cin >> n >> k;
+    int boxes, cakes;
+    cin >> cakes >> boxes;
 
-    for (int i = 1; i <= n; i++) { 
+    for (int i = 0; i <= cakes - 1; i++) { 
         cin >> a[i]; 
     }
 
-    for (int i = 1; i <= n; last[a[i]] = i, i++) {
-        for (int j = 1; j <= k; j++) {
-            t[j - 1].modify(1, 0, n, last[a[i]], i - 1, 1);
-            f[i][j] = t[j - 1].query(1, 0, n, 0, i - 1);
-            t[j].modify(1, 0, n, i, i, f[i][j]);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
+            dp[i][j] = -1;
         }
-    }
-    
-    return printf("%d", f[n][k]), 0;
+    } 
+
+    cout << recurse(boxes - 1, cakes - 1) << "\n";
+
+    // for (int i = 0; i < boxes; i++) {
+    //     for (int j = 0; j < cakes; j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
 }
